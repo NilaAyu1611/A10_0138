d5f1888ff8642d9378816508e8bb8c61d1070e2d
NilaAyu1611 <nilaayucw@gmail.com>
2025-01-28 15:02:39 +0700

    Membuat implementasi dari AktivitaspertanianRepository

--- app/src/main/java/com/example/uaspam/repository/AktivitaspertanianRepository.kt
+++ app/src/main/java/com/example/uaspam/repository/AktivitaspertanianRepository.kt
@@ -1,6 +1,8 @@
 package com.example.uaspam.repository
 
 import com.example.uaspam.model.AktivitasPertanian
+import com.example.uaspam.servive_api.AktivitaspertanianService
+import java.io.IOException
 
 interface AktivitaspertanianRepository {
     suspend fun getAktivitasPertanian(): List<AktivitasPertanian>
@@ -8,4 +10,36 @@ interface AktivitaspertanianRepository {
     suspend fun updateAktivitasPertanian(id_aktivitas: String, aktivitasPertanian: AktivitasPertanian)
     suspend fun deleteAktivitasPertanian(id_aktivitas: String)
     suspend fun getAktivitasPertanianById(id_aktivitas: String): AktivitasPertanian
+}
+
+class NetworkaktivitaspertanianRepository(
+    private val aktivitaspertanianApiService: AktivitaspertanianService
+) : AktivitaspertanianRepository {
+    override suspend fun insertAktivitasPertanian(aktivitasPertanian: AktivitasPertanian) {
+        aktivitaspertanianApiService.insertAktivitasPertanian(aktivitasPertanian)
+    }
+
+    override suspend fun updateAktivitasPertanian(id_aktivitas: String, aktivitasPertanian: AktivitasPertanian) {
+        aktivitaspertanianApiService.updateAktivitasPertanian(id_aktivitas,aktivitasPertanian)
+    }
+
+    override suspend fun deleteAktivitasPertanian(id_aktivitas: String){
+        try {
+            val response = aktivitaspertanianApiService.deleteAktivitasPertanian(id_aktivitas)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete AktivitasPertanian. HTTP Status code: ${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+
+    override suspend fun getAktivitasPertanian(): List<AktivitasPertanian> = aktivitaspertanianApiService.getAktivitasPertanian()
+    override suspend fun getAktivitasPertanianById(id_aktivitas: String): AktivitasPertanian {
+        return aktivitaspertanianApiService.getAktivitasPertanianById(id_aktivitas)
+    }
 }
