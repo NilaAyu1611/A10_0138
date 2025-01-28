281b26acaed7866e1a99989d33e29897eea44931
NilaAyu1611 <nilaayucw@gmail.com>
2025-01-28 14:59:01 +0700

    Membuat implementasi dari PekerjaRepository

--- app/src/main/java/com/example/uaspam/repository/PekerjaRepository.kt
+++ app/src/main/java/com/example/uaspam/repository/PekerjaRepository.kt
@@ -1,6 +1,8 @@
 package com.example.uaspam.repository
 
 import com.example.uaspam.model.Pekerja
+import com.example.uaspam.servive_api.PekerjaService
+import java.io.IOException
 
 interface PekerjaRepository {
     suspend fun getPekerja(): List<Pekerja>
@@ -8,4 +10,35 @@ interface PekerjaRepository {
     suspend fun updatePekerja(id_pekerja: String, pekerja: Pekerja)
     suspend fun deletePekerja(id_pekerja: String)
     suspend fun getPekerjaById(id_pekerja: String): Pekerja
+}
+
+class NetworkpekerjaRepository(
+    private val pekerjaApiService: PekerjaService
+) : PekerjaRepository {
+    override suspend fun insertPekerja(pekerja: Pekerja) {
+        pekerjaApiService.insertPekerja(pekerja)
+    }
+
+    override suspend fun updatePekerja(id_pekerja: String, pekerja: Pekerja) {
+        pekerjaApiService.updatePekerja(id_pekerja, pekerja)
+    }
+
+    override suspend fun deletePekerja(id_pekerja: String) {
+        try {
+            val response = pekerjaApiService.deletePekerja(id_pekerja)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete Pekerja. HTTP Status code: ${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getPekerja(): List<Pekerja> = pekerjaApiService.getPekerja()
+    override suspend fun getPekerjaById(id_pekerja: String):Pekerja {
+        return pekerjaApiService.getPekerjaById(id_pekerja)
+    }
 }
