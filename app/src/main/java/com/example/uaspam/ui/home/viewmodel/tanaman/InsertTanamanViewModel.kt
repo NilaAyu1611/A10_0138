387c379591c6633520b458dbeda3f9410a3d6ec4
NilaAyu1611 <nilaayucw@gmail.com>
2025-01-29 10:44:18 +0700

    Membuat data class uistate tanaman

--- app/src/main/java/com/example/uaspam/ui/home/viewmodel/tanaman/InsertTanamanViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/home/viewmodel/tanaman/InsertTanamanViewModel.kt
@@ -1,6 +1,14 @@
 package com.example.uaspam.ui.home.viewmodel.tanaman
 
 
+
+
+// Menyimpan state form input
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent() // State default berisi objek kosong dari InsertUiEvent
+)
+
+
 // Menyimpan data input pengguna untuk form tanaman
 data class InsertUiEvent(
     val id_tanaman: String = "",
