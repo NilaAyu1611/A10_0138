fa70e765da68dff3062d0fa5324713eacb9195f2
NilaAyu1611 <nilaayucw@gmail.com>
2025-01-29 11:14:52 +0700

    Membuat class UpdateAktivitasPertanianViewModel

--- app/src/main/java/com/example/uaspam/ui/home/viewmodel/aktivitaspertanian/UpdateAktivitasPertanianViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/home/viewmodel/aktivitaspertanian/UpdateAktivitasPertanianViewModel.kt
@@ -1,6 +1,65 @@
 package com.example.uaspam.ui.home.viewmodel.aktivitaspertanian
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.uaspam.model.AktivitasPertanian
+import com.example.uaspam.repository.AktivitaspertanianRepository
+import kotlinx.coroutines.launch
+
+// ViewModel untuk mengatur data dan logika form update mahasiswa
+class UpdateAktivitasPertanianViewModel(private val ak: AktivitaspertanianRepository) : ViewModel() {
+
+    // Data untuk menyimpan keadaan form (seperti input dari pengguna)
+    var updateakUiState by mutableStateOf(UpdateakUiState())
+        private set
+
+    // Fungsi untuk mendapatkan data mahasiswa berdasarkan id
+    fun getAktivitasPertanianById(id_aktivitas: String) {
+        viewModelScope.launch {
+            try {
+                val aktivitasPertanian = ak.getAktivitasPertanianById(id_aktivitas  )
+                updateakUiState = updateakUiState.copy(
+                    updateakUiEvent = UpdateakUiEvent(
+                        id_aktivitas =aktivitasPertanian.id_aktivitas,
+                        id_tanaman =aktivitasPertanian.id_tanaman,
+                        id_pekerja =aktivitasPertanian.id_pekerja,
+                        tanggal_aktivitas = aktivitasPertanian.tanggal_aktivitas,
+                        deskripsi_aktivitas = aktivitasPertanian.deskripsi_aktivitas
+                    )
+                )
+            } catch (e: Exception) {
+                // Handle error jika diperlukan
+            }
+        }
+    }
+
+    // Fungsi untuk mengubah data form ketika ada input dari pengguna
+    fun updateAkState(updateakUiEvent: UpdateakUiEvent) {
+        updateakUiState = UpdateakUiState(updateakUiEvent = updateakUiEvent) // Perbarui data berdasarkan event
+    }
+
+    // Fungsi untuk memuat data mahasiswa ke dalam form untuk di-update
+    fun loadAktivitasPertanianData(aktivitasPertanian: AktivitasPertanian) {
+        updateakUiState = aktivitasPertanian.toUpdateakUiStateAk()
+    }
+
+    // Fungsi untuk memperbarui data mahasiswa ke database
+    fun updateAk() {
+        viewModelScope.launch {
+            try {
+                val aktivitasPertanian = updateakUiState.updateakUiEvent.toAk()
+                ak.updateAktivitasPertanian(updateakUiState.updateakUiEvent.id_aktivitas, aktivitasPertanian)
+                println("Update berhasil: $aktivitasPertanian")
+            } catch (e: Exception) {
+                e.printStackTrace() // Log error jika ada
+            }
+        }
+    }
+
+}
 
 
 // Menyimpan state form update mahasiswa
