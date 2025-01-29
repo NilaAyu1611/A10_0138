acf293fa4ed75ee9231db5ab41edf5414a2919a9
NilaAyu1611 <nilaayucw@gmail.com>
2025-01-29 10:49:53 +0700

    Membuat  class dan fungsi untuk UpdateTanamanViewModel

--- app/src/main/java/com/example/uaspam/ui/home/viewmodel/tanaman/UpdateTanamanViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/home/viewmodel/tanaman/UpdateTanamanViewModel.kt
@@ -1,6 +1,65 @@
 package com.example.uaspam.ui.home.viewmodel.tanaman
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.uaspam.model.Tanaman
+import com.example.uaspam.repository.TanamanRepository
+import kotlinx.coroutines.launch
+
+// ViewModel untuk mengatur data dan logika form update tnm
+class UpdateTanamanViewModel(private val tnm: TanamanRepository) : ViewModel() {
+
+    // Data untuk menyimpan keadaan form (seperti input dari pengguna)
+    var updateUiState by mutableStateOf(UpdateUiState())
+        private set
+
+    // Fungsi untuk mendapatkan data mahasiswa berdasarkan NIM
+    fun getTanamanById(id_tanaman: String) {
+        viewModelScope.launch {
+            try {
+                val tanaman = tnm.getTanamanById(id_tanaman)
+                updateUiState = updateUiState.copy(
+                    updateUiEvent = UpdateUiEvent(
+                        id_tanaman = tanaman.id_tanaman,
+                        nama_tanaman = tanaman.nama_tanaman,
+                        periode_tanam = tanaman.periode_tanam,
+                        deskripsi_tanaman = tanaman.deskripsi_tanaman
+                    )
+                )
+            } catch (e: Exception) {
+                // Handle error jika diperlukan
+            }
+        }
+    }
+
+    // Fungsi untuk mengubah data form ketika ada input dari pengguna
+    fun updateTnmState(updateUiEvent: UpdateUiEvent) {
+        updateUiState = UpdateUiState(updateUiEvent = updateUiEvent) // Perbarui data berdasarkan event
+    }
+
+    // Fungsi untuk memuat data mahasiswa ke dalam form untuk di-update
+    fun loadTanamanData(tanaman: Tanaman) {
+        updateUiState = tanaman.toUpdateUiStateTnm()
+    }
+
+    // Fungsi untuk memperbarui data mahasiswa ke database
+    fun updateTnm() {
+        viewModelScope.launch {
+            try {
+                val tanaman = updateUiState.updateUiEvent.toTnm()
+                tnm.updateTanaman(updateUiState.updateUiEvent.id_tanaman, tanaman)
+                println("Update berhasil: $tanaman")
+            } catch (e: Exception) {
+                e.printStackTrace() // Log error jika ada
+            }
+        }
+    }
+
+}
+
 
 // Menyimpan state form update mahasiswa
 data class UpdateUiState(
