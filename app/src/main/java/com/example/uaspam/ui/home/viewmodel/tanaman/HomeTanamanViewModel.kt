cee01892c482b87568b59d0fb1cbb781ff6866fd
NilaAyu1611 <nilaayucw@gmail.com>
2025-01-29 10:41:54 +0700

    Membuat class HomeTanamanViewModel

--- app/src/main/java/com/example/uaspam/ui/home/viewmodel/tanaman/HomeTanamanViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/home/viewmodel/tanaman/HomeTanamanViewModel.kt
@@ -1,6 +1,15 @@
 package com.example.uaspam.ui.home.viewmodel.tanaman
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.uaspam.model.Tanaman
+import com.example.uaspam.repository.TanamanRepository
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 sealed class HomeUiState{ // Digunakan untuk membatasi subclass yang dapat di-extend dari kelas ini.
 
@@ -12,4 +21,57 @@ sealed class HomeUiState{ // Digunakan untuk membatasi subclass yang dapat di-ex
 
     // Subclass Loading. Menunjukkan bahwa aplikasi sedang dalam proses memuat data.
     object Loading: HomeUiState()
+}
+
+
+class HomeTanamanViewModel(private val tnm: TanamanRepository): ViewModel() {
+
+    // tnmUiState digunakan untuk menyimpan keadaan UI (state) tnm.
+    // mutableStateOf digunakan untuk membuat state yang dapat berubah dan otomatis memicu pembaruan UI ketika nilainya berubah.
+    // State awalnya diset ke HomeUiState.Loading.
+    var tnmUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set // Setter-nya dibuat private agar state hanya dapat diubah oleh ViewModel.
+
+    init {
+        getTnm()
+    }
+
+    fun getTnm() {
+        viewModelScope.launch {
+
+            // Set state ke Loading untuk menunjukkan bahwa data sedang diproses.
+            tnmUiState = HomeUiState.Loading
+
+            // Mencoba mengambil data tanaman dari repository menggunakan blok try-catch.
+            tnmUiState = try {
+
+                // Jika berhasil, state diubah menjadi Success dengan daftar mahasiswa sebagai datanya.
+                HomeUiState.Success(tnm.getTanaman())
+            }catch (e: IOException) {
+
+                // Jika terjadi kesalahan jaringan atau I/O, set state ke Error.
+                HomeUiState.Error
+            }catch (e: HttpException) {
+
+                // Jika terjadi kesalahan HTTP (misalnya, 404 atau 500), set state ke Error.
+                HomeUiState.Error
+            }
+        }
+    }
+
+    fun deleteTnm(id_tanaman: String) {
+        viewModelScope.launch {
+
+            // Menggunakan blok try-catch untuk menangani kemungkinan kesalahan selama proses penghapusan.
+            try {
+
+                // Memanggil fungsi deleteTanaman pada repository untuk menghapus data mahasiswa berdasarkan NIM.
+                tnm.deleteTanaman(id_tanaman)
+            }catch (e: IOException) {
+                HomeUiState.Error
+            }catch (e: HttpException) {
+                HomeUiState.Error
+            }
+        }
+    }
 }
