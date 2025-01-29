0794033abc03d2d3b47c0d6efeaecd6d041a272d
NilaAyu1611 <nilaayucw@gmail.com>
2025-01-29 10:44:56 +0700

    Membuat fungsi tanaman tampilannya

--- app/src/main/java/com/example/uaspam/ui/home/viewmodel/tanaman/InsertTanamanViewModel.kt
+++ app/src/main/java/com/example/uaspam/ui/home/viewmodel/tanaman/InsertTanamanViewModel.kt
@@ -1,6 +1,6 @@
 package com.example.uaspam.ui.home.viewmodel.tanaman
 
-
+import com.example.uaspam.model.Tanaman
 
 
 // Menyimpan state form input
@@ -15,4 +15,28 @@ data class InsertUiEvent(
     val nama_tanaman: String = "",
     val periode_tanam: String = "",
     val deskripsi_tanaman: String = ""
+)
+
+
+// Fungsi untuk mengubah data InsertUiEvent menjadi
+fun InsertUiEvent.toTnm(): Tanaman = Tanaman( // InsertUiEvent > Mahasiswa > Simpan data Mhs ke db
+    id_tanaman = id_tanaman,
+    nama_tanaman=nama_tanaman,
+    periode_tanam=periode_tanam,
+    deskripsi_tanaman = deskripsi_tanaman
+
+)
+
+// Fungsi untuk mengubah data Mahasiswa menjadi InsertUiState
+fun Tanaman.toInsertUiStateTnm(): InsertUiState = InsertUiState( // Mahasiswa > insertUiEvent > Masuk ke InsertUiState
+    insertUiEvent = toInsertUiEvent() // Memanggil fungsi toInsertUiEvent untuk mengonversi data Mahasiswa
+)
+
+// Fungsi untuk mengubah data Mahasiswa menjadi data InsertUiEvent
+fun Tanaman.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    id_tanaman = id_tanaman,
+    nama_tanaman=nama_tanaman,
+    periode_tanam=periode_tanam,
+    deskripsi_tanaman = deskripsi_tanaman
+
 )
